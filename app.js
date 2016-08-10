d333242970d5ae0b5138a52f7db10c761f5729ae
scmdesai <scmdesai@gmail.com>
2016-08-09 21:46:52 -0500

    signup

--- app.js
+++ app.js
@@ -68084,7 +68084,6 @@ Ext.define('Ext.picker.Picker', {
                                 success: function(result, request) {
                                     var jsonData = Ext.util.JSON.decode(result.responseText);
                                     //var resultMessage = jsonData.data.result;
-                                    console.log(result.responseText[0]);
                                     storeUserDetails.add({
                                         'customerId': record.get('customerId'),
                                         'email': email,
