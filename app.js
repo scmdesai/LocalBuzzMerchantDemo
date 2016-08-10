76e7d51965a051d67cf76324eecdd14c5413927b
scmdesai <scmdesai@gmail.com>
2016-08-10 11:45:05 -0500

    respons

--- app.js
+++ app.js
@@ -70385,27 +70385,33 @@ Ext.application({
         FacebookInAppBrowser.settings.permissions = 'email';
         // Optional
         FacebookInAppBrowser.settings.timeoutDuration = 7500;
-        FacebookInAppBrowser.getInfo(function(response) {
-            if (response) {
-                if (!Ext.Viewport.getComponent('Login')) {
-                    var view = Ext.Viewport.add({
-                            xtype: 'Login'
-                        });
-                    Ext.Viewport.setActiveItem(view);
-                } else {
-                    Ext.Viewport.setActiveItem(Ext.Viewport.getComponent('Login'));
+        var res = FacebookInAppBrowser.getInfo(function(response) {
+                if (response) {
+                    return true;
+                } else  {
+                    return false;
                 }
+                
+            });
+        if (res === "true") {
+            if (!Ext.Viewport.getComponent('Login')) {
+                var view = Ext.Viewport.add({
+                        xtype: 'Login'
+                    });
+                Ext.Viewport.setActiveItem(view);
             } else {
-                if (!Ext.Viewport.getComponent('WelcomeScreen')) {
-                    var view = Ext.Viewport.add({
-                            xtype: 'WelcomeScreen'
-                        });
-                    Ext.Viewport.setActiveItem(view);
-                } else {
-                    Ext.Viewport.setActiveItem(Ext.Viewport.getComponent('WelcomeScreen'));
-                }
+                Ext.Viewport.setActiveItem(Ext.Viewport.getComponent('Login'));
             }
-        });
+        } else {
+            if (!Ext.Viewport.getComponent('WelcomeScreen')) {
+                var view1 = Ext.Viewport.add({
+                        xtype: 'WelcomeScreen'
+                    });
+                Ext.Viewport.setActiveItem(view1);
+            } else {
+                Ext.Viewport.setActiveItem(Ext.Viewport.getComponent('WelcomeScreen'));
+            }
+        }
         document.addEventListener("resume", Ext.bind(onResume, this), false);
         function onResume(e) {}
     }
