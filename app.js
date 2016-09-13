e4a4b472610acac7d0cfd8abec06044e31d65520
scmdesai <scmdesai@gmail.com>
2016-09-13 00:33:41 -0500

    dealpicture

--- app.js
+++ app.js
@@ -67667,6 +67667,19 @@ Ext.define('Ext.picker.Picker', {
                 xtype: 'container',
                 cls: 'contact-name',
                 disabled: true,
+                hidden: false,
+                id: 'nameTxt4',
+                itemId: 'nameTxt4',
+                margin: '10 5 5 5',
+                minHeight: '8vh',
+                style: 'word-wrap:break-word;font-family:Arial;font-size:3vw',
+                top: '39vh',
+                width: '98%'
+            },
+            {
+                xtype: 'container',
+                cls: 'contact-name',
+                disabled: true,
                 html: '<p style="font-size:3vw;text-align:center">       Published through Local Buzz',
                 id: 'nameTxt2',
                 itemId: 'nameTxt2',
@@ -67795,11 +67808,13 @@ Ext.define('Ext.picker.Picker', {
     onDealPictureShow: function(component, eOpts) {
         var record = Ext.getStore('LocalStore').getAt(0);
         if (record.get('dealImageURL')) {
-            this.down('#dealimage').setHtml('<img src="' + record.get('dealImageURL') + '" style="margin: 0px 5px 5px 0px;height:37vh;width:98%;border:none;"/><br><div style="font-size:4vw;">' + record.get('dealDescription') + '</div>');
+            this.down('#dealimage').setHtml('<img src="' + record.get('dealImageURL') + '" style="margin: 0px 5px 5px 0px;height:38vh;width:98%;border:none;"/><br><br><div style="font-size:4vw;">' + record.get('dealDescription') + '</div>');
             this.down('#nameTxt3').show();
+            this.down('#nameTxt4').show();
         } else {
             this.down('#dealimage').setHtml('<img src="resources/img/localbuzzicon.png" align="right" style="margin: 5px 5px 5px 5px"/><br><div style="font-size:6vw;">' + record.get('dealName') + '</div><br><br><div style="font-size:4vw;">' + record.get('dealDescription') + '</div><br><div style="font-size:3vw;">Valid ' + record.get('dealStartDate') + ' - ' + record.get('dealEndDate') + '</div>');
             this.down('#nameTxt3').hide();
+            this.down('#nameTxt4').hide();
         }
     },
     setRecord: function(record) {
@@ -67808,6 +67823,7 @@ Ext.define('Ext.picker.Picker', {
             var name = record.get('itemName');
             var businessName = record.get('businessName');
             this.down('#nameTxt1').setHtml(record.get('businessName'));
+            this.down('#nameTxt4').setHtml(record.get('dealDescription'));
             //var store = Ext.getStore('MyJsonPStore');
             //  var rec = store.findRecord('businessName', businessName);
             var rec = Ext.getStore('UserDetails').getAt(0);
