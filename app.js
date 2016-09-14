e31c21aff3426008c7664cdfa69a680fd3136a96
scmdesai <scmdesai@gmail.com>
2016-09-13 19:56:52 -0500

    regex

--- app.js
+++ app.js
@@ -67689,7 +67689,8 @@ Ext.define('Ext.picker.Picker', {
                                 var dealDescription = record.get('dealDescription').toString();
                                 console.log('dealDescription is : ' + dealDescription);
                                 var regex = new RegExp("(http|ftp|https)://[w-]+(.[w-]+)+([w.,@?^=%&amp;:/~+#-]*[w@?^=%&amp;/~+#-])?");
-                                var url = regex.exec(dealDescription);
+                                //var url = regex.exec(dealDescription);
+                                var url = dealDescription.match(regex);
                                 console.log('url is ' + url[0]);
                                 window.open(url[0], '_system', 'location=yes');
                             });
