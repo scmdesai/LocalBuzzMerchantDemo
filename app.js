99236ad1ba3aebb40e13d4231752e752a7193803
scmdesai <scmdesai@gmail.com>
2016-09-13 20:10:12 -0500

    url

--- app.js
+++ app.js
@@ -67688,7 +67688,8 @@ Ext.define('Ext.picker.Picker', {
                                 var record = Ext.getStore('LocalStore').getAt(0);
                                 var dealDescription = record.get('dealDescription').toString();
                                 console.log('dealDescription is : ' + dealDescription);
-                                var regex = new RegExp("(http|ftp|https)://[w-]+(.[w-]+)+([w.,@?^=%&amp;:/~+#-]*[w@?^=%&amp;/~+#-])?");
+                                // var regex =new RegExp("(http|ftp|https)://[w-]+(.[w-]+)+([w.,@?^=%&amp;:/~+#-]*[w@?^=%&amp;/~+#-])?");
+                                var regex = new RegExp("[a-z]+[:.].*?(?=s)");
                                 //var url = regex.exec(dealDescription);
                                 var url = dealDescription.match(regex);
                                 console.log('url is ' + url);
