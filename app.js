9dc88f99f6388c411f11247d7e1ec57d68be8683
scmdesai <scmdesai@gmail.com>
2016-09-15 22:16:51 -0500

    video

--- app.js
+++ app.js
@@ -67964,7 +67964,7 @@ Ext.define('Ext.picker.Picker', {
                 this.down('#dealimage').setHtml('<div><img src="' + record.get('dealImageURL') + '" style="height:39vh;width:98%;display:inline;border:none;"/><p id="enlargebtn" class="icon-enlarge" style="background:none;position:absolute;bottom: 1.5em; right: 1.5em"></p></div>');
             } else {
                 if (Ext.os.is('Android')) {
-                    // this.down('#dealimage').setHtml('<div><video style="height:39vh;width:100%;" videoWidth=98% videoHeight= 39vh controls preload="auto" onclick="this.play();" > <source src="'+record.get('dealImageURL')+'" type="video/mp4"><param name="allowFullScreen" value="true"></video></div>');
+                    this.down('#dealimage').setHtml('<div><video style="height:39vh;width:100%;" videoWidth=98% videoHeight= 39vh controls preload="auto" onclick="window.plugins.html5Video.play("video1");" > <source src="' + record.get('dealImageURL') + '" type="video/mp4"><param name="allowFullScreen" value="true"></video></div>');
                     //window.plugins.html5Video.initialize( { "video1": '"'+record.get('dealImageURL')+'"'} );
                     window.plugins.html5Video.play("video1");
                 } else {
