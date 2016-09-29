4f96843982190b8e824abfb7996ef5b38c4a690f
scmdesai <scmdesai@gmail.com>
2016-09-28 23:56:11 -0500

    element

--- app.js
+++ app.js
@@ -69777,7 +69777,7 @@ Ext.define('Ext.picker.Picker', {
                 listeners: [
                     {
                         fn: function(component, eOpts) {
-                            component.setYearTo(parseInt(new Date().getFullYear() + 1));
+                            component.element.setYearTo(parseInt(new Date().getFullYear() + 1));
                         },
                         event: 'initialize'
                     }
