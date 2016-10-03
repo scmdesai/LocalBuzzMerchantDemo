7c58fff431065428317209d66c6a1e5d7d23e0e5
scmdesai <scmdesai@gmail.com>
2016-10-03 13:13:20 -0500

    test

--- app.js
+++ app.js
@@ -67712,7 +67712,7 @@ Ext.define('Ext.picker.Picker', {
                     'dealimage',
                     'w3-card-4',
                     'w3-container',
-                    'w3-theme-l4'
+                    'w3-theme-l1'
                 ],
                 disabled: true,
                 docked: 'top',
