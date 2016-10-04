f8a089b13a9ffb05997e12fdf584c142930f1e9b
scmdesai <scmdesai@gmail.com>
2016-10-03 23:18:49 -0500

    Revert "style"
    This reverts commit 6efdfb4dcb37114db16b9a2dc3a4ab2f0ef55137.

--- app.js
+++ app.js
@@ -66317,12 +66317,11 @@ Ext.define('Ext.picker.Picker', {
                 xtype: 'component',
                 docked: 'top',
                 hidden: false,
-                html: '<center><img src ="resources/img/localbuzzicon.png"/><br><div style="text-align:center;"><h3 style="color:#00529D;font-family:Arial;font-size:6vw;text-align:center"><b>Welcome to <br><br>Local Buzz For Merchants</h3></div></center>',
+                html: '<center><img src ="resources/img/localbuzzicon.png"/><br><div style="text-align:center;"><h3 style="color:#00529D"><b>Welcome to <br><br>Local Buzz For Merchants</h3></div></center>',
                 id: 'text1',
                 style: 'word-wrap:break-word;font-family:Arial;font-size:6vw;background:url(resources/img/whitetexture.png);',
                 styleHtmlContent: true,
-                top: '7%',
-                width: '100%'
+                top: '7%'
             },
             {
                 xtype: 'button',
