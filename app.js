64ae2fe81dc44545b78799ea860d50e3ba757fb5
scmdesai <scmdesai@gmail.com>
2016-10-03 23:32:27 -0500

    cls

--- app.js
+++ app.js
@@ -66307,11 +66307,8 @@ Ext.define('Ext.picker.Picker', {
         style: 'background:url(resources/img/whitetexture.png);',
         styleHtmlContent: true,
         width: '100%',
+        layout: 'fit',
         scrollable: false,
-        layout: {
-            type: 'vbox',
-            align: 'stretchmax'
-        },
         items: [
             {
                 xtype: 'component',
@@ -66319,9 +66316,9 @@ Ext.define('Ext.picker.Picker', {
                 hidden: false,
                 html: '<center><img src ="resources/img/localbuzzicon.png"/><br><div style="text-align:center;"><h3 style="color:#00529D;font-family:Arial;font-size:6vw;text-align:center"><b>Welcome to <br><br>Local Buzz For Merchants</h3></div></center>',
                 id: 'text1',
-                margin: '100 0 0 0',
                 style: 'word-wrap:break-word;font-family:Arial;font-size:6vw;background:url(resources/img/whitetexture.png);',
                 styleHtmlContent: true,
+                top: '7%',
                 width: '100%'
             },
             {
@@ -66340,6 +66337,7 @@ Ext.define('Ext.picker.Picker', {
                 left: '20%',
                 margin: '50 0 0 0',
                 style: 'font-size:5vw;font-family:Arial',
+                top: '35%',
                 ui: 'action',
                 width: '60%',
                 text: 'Login'
